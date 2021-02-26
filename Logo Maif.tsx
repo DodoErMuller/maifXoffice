62f981a7cec08e0805e95abea41f30ea5290e416
DodoErMuller <67642298+DodoErMuller@users.noreply.github.com>
2021-02-26 13:13:48 +0000

    Deploying to gh-pages from master @ ed48398df02815bdde4b9e3526a10e5d1498ed15 🚀

---
+++ Logo Maif.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Logo MAIF" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="MAIF_Logo_lettre_blanche_rvb_ 256.jpg" trans="ffffff" width="240" height="202"/>
+</tileset>
